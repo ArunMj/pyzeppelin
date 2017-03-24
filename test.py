e6cbca83b9f0da0ffb3e8bd724550ead266ae730
arun.muraleedharan <arun.muraleedharan@flytxt.com>
2017-03-24 19:49:33 +0530

    test via nc

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from zeppelin import ZeppelinClient
 
 
-zepp = ZeppelinClient(host='localhost', port=8080, auth=None)
+zepp = ZeppelinClient(host='localhost', port=8088, auth=None)
 
 print "getting all notebooks"
 print zepp.list_notebooks()
